59818037d1296833901a8173dcc706e5dd0d2d58
Kwabena Aning <kwabena@mykobo.co>
2026-01-22 20:48:49 +0000

    fix: To json on fee configuration
    Signed-off-by: Kwabena Aning <kwabena@mykobo.co>

--- mykobo_py/business/models.py
+++ mykobo_py/business/models.py
@@ -9,8 +9,8 @@ from dataclasses_json.api import dataclass_json
 class FeeConfiguration:
     transaction_type: str
     fee_rate: float
-    effective_from: datetime
+    effective_from: str
     client_domain: str
-    effective_until: Optional[datetime]
+    effective_until: Optional[str]
     created_by: str
     change_reason: str
